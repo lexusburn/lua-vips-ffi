8ba3776cf1caa6a8c17b00f3f0bfbf43a3dc49c5
Frederic Biermann <mail@codingninja.de>
2017-05-22 23:26:39 +0200

    merge with latest version from jcupitt

--- experiment/test-vimage.lua
+++ experiment/test-vimage.lua
@@ -12,7 +12,7 @@ image.frank(1, 2, 3)
 
 image2 = image:invert()
 
-image3 = vimage:black(1, 2, {bands = 3})
+image3 = vimage.black(1, 2, {bands = 3})
 
 image4 = vimage.new_from_file("/data/images/secondImg.png")
 image4 = image4:invert()
--- experiment/voperation_connector.lua
+++ experiment/voperation_connector.lua
@@ -1,173 +1,223 @@
--- manage VipsObject
--- abstract base class for operation and image
+-- manage VipsOperation
+-- lookup and call operations
 
 local ffi = require("ffi")
 local gvalue = require("gvalue_connector")
+local vobject = require("vobject_connector")
+local bit = require("bit")
+local band = bit.band
 local vips = ffi.load("vips")
 
 ffi.cdef[[
-    typedef struct _GObject {
-        int g_type_instance;
-        unsigned ref_count;
-        void *qdata;
-    } GObject;
-
-    typedef struct _VipsObject {
-        GObject parent_object;
-        bool constructed;
-        bool static_object;
-        void *argument_table;
-        char *nickname;
-        char *description;
-        bool preclose;
-        bool close;
-        bool postclose;
-        size_t local_memory;
-    } VipsObject;
-
-    typedef struct _VipsObjectClass {
-        // opaque
-    } VipsObjectClass;
-
-    typedef struct _GParamSpec {
-        void* g_type_instance;
-
-        const char* name;
-        unsigned int flags;
-        unsigned long int value_type;
-        unsigned long int owner_type;
-
-        // rest opaque
-    } GParamSpec;
-
-    typedef struct _VipsArgument {
-        GParamSpec *pspec;
-    } VipsArgument;
-
-    typedef struct _VipsArgumentInstance {
-        VipsArgument parent;
+    typedef struct _VipsOperation {
+        VipsObject parent_instance;
 
         // opaque
-    } VipsArgumentInstance;
+    } VipsOperation;
 
-    typedef enum _VipsArgumentFlags {
-        VIPS_ARGUMENT_NONE = 0,
-        VIPS_ARGUMENT_REQUIRED = 1,
-        VIPS_ARGUMENT_CONSTRUCT = 2,
-        VIPS_ARGUMENT_SET_ONCE = 4,
-        VIPS_ARGUMENT_SET_ALWAYS = 8,
-        VIPS_ARGUMENT_INPUT = 16,
-        VIPS_ARGUMENT_OUTPUT = 32,
-        VIPS_ARGUMENT_DEPRECATED = 64,
-        VIPS_ARGUMENT_MODIFY = 128
-    } VipsArgumentFlags;
+    VipsOperation* vips_operation_new (const char* name);
 
-    typedef struct _VipsArgumentClass {
-        VipsArgument parent;
+    typedef void *(*VipsArgumentMapFn) (VipsOperation* object,
+        GParamSpec* pspec,
+        VipsArgumentClass* argument_class,
+        VipsArgumentInstance* argument_instance,
+        void* a, void* b);
 
-        VipsObjectClass *object_class;
-        VipsArgumentFlags flags;
-        int priority;
-        unsigned long int offset;
-    } VipsArgumentClass;
+    void* vips_argument_map (VipsOperation* object,
+        VipsArgumentMapFn fn, void* a, void* b);
 
-    void g_object_unref (void* object);
-
-    int vips_object_get_argument (VipsObject* object,
-        const char *name, GParamSpec** pspec,
-        VipsArgumentClass** argument_class,
-        VipsArgumentInstance** argument_instance);
-
-    void g_object_set_property (VipsObject* object,
-        const char *name, GValue* value);
-    void g_object_get_property (VipsObject* object,
-        const char* name, GValue* value);
-
-    const char* vips_error_buffer (void);
-    void vips_error_clear (void);
+    VipsOperation* vips_cache_operation_build (VipsOperation* operation);
+    void vips_object_unref_outputs (VipsOperation *operation);
 
 ]]
 
-local vobject
-local vobject_mt = {
-    -- no __gc method, we don't build these things ourselves, just wrap the
-    -- pointer, so we use ffi.gc() instead
-    __index = {
-        -- types to get ref back from vips_object_get_argument
-        typeof = ffi.typeof("VipsObject*"),
-        pspec_typeof = ffi.typeof("GParamSpec*[1]"),
-        argument_class_typeof = ffi.typeof("VipsArgumentClass*[1]"),
-        argument_instance_typeof = ffi.typeof("VipsArgumentInstance*[1]"),
+function print_r(t)
+    local print_r_cache = {}
+    local function sub_print_r(t, indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent .. "*" .. tostring(t))
+        else
+            print_r_cache[tostring(t)] = true
+            if type(t) == "table" then
+                for pos, val in pairs(t) do
+                    if type(val) == "table" then
+                        print(indent ..
+                                "[" .. pos .. "] => " ..  tostring(t) .. " {")
+                        sub_print_r(val, indent ..
+                                string.rep(" ", string.len(pos) + 8))
+                        print(indent ..
+                                string.rep(" ", string.len(pos) + 6) .. "}")
+                    elseif type(val) == "string" then
+                        print(indent .. "[ ".. pos .. '] => "' .. val .. '"')
+                    else
+                        print(indent .. "[" .. pos .. "] => " .. tostring(val))
+                    end
+                end
+            else
+                print(indent .. tostring(t))
+            end
+        end
+    end
+    if type(t) == "table" then
+        print(tostring(t) .. " {")
+        sub_print_r(t, "  ")
+        print("}")
+    else
+        sub_print_r(t, "  ")
+    end
+    print()
+end
 
-        new = function(object)
-            print("vobject.new")
-            print("  ptr =", object)
-            ffi.gc(object,
-                function(x)
-                    print("unreffing", x)
-                    vips.g_object_unref(x)
+local REQUIRED = 1
+local CONSTRUCT = 2
+local SET_ONCE = 4
+local SET_ALWAYS = 8
+local INPUT = 16
+local OUTPUT = 32
+local DEPRECATED = 64
+local MODIFY = 128
+
+local voperation
+local voperation_mt = {
+    __index = {
+        argumentmap_typeof = ffi.typeof("VipsArgumentMapFn"),
+
+        -- cast to an object
+        object = function(self)
+            return ffi.cast(vobject.typeof, self)
+        end,
+
+        -- this is slow ... call as little as possible
+        getargs = function (self)
+            local args = {}
+            local cb = ffi.cast(voperation.argumentmap_typeof,
+                function(self, pspec, argument_class, argument_instance, a, b)
+                    table.insert(args,
+                        {name = ffi.string(pspec.name),
+                            flags = tonumber(argument_class.flags)
+                        }
+                    )
                 end
             )
-            return object
-        end,
-        get_type = function(object, name)
-            local pspec = vobject.pspec_typeof()
-            local argument_class = vobject.argument_class_typeof()
-            local argument_instance = vobject.argument_instance_typeof()
-            local result = vips.vips_object_get_argument(object, name,
-                pspec, argument_class, argument_instance)
+            vips.vips_argument_map(self, cb, nil, nil )
+            cb:free()
 
-            if result ~= 0 then
+            return args
+        end,
+
+        call = function(name, ...)
+            local call_args = {...}
+
+            local operation = vips.vips_operation_new(name)
+            if operation == nil then
+                print("no such operation", name)
+                return
+            end
+            local object = operation:object()
+            object:new()
+
+            local arguments = operation:getargs()
+
+            print(name, "needs:")
+            print_r(arguments)
+
+            print("passed:")
+            print_r(call_args)
+
+            local n = 0
+            for i = 1, #arguments do
+                local flags = arguments[i].flags
+
+                if band(flags, INPUT) ~= 0 and
+                        band(flags, REQUIRED) ~= 0 and
+                        band(flags, DEPRECATED) == 0 then
+                    n = n + 1
+                    if not object:set(arguments[i].name, call_args[n]) then
+                        return
+                    end
+                end
+            end
+
+            local last_arg
+            if #call_args == n then
+                last_arg = nil
+            elseif #call_args == n + 1 then
+                last_arg = call_args[#call_args]
+                if type(last_arg) ~= "table" then
+                    error("final argument is not a table")
+                end
+            else
+                print("#call_args =", #call_args)
+                print("n =", n)
+                error("wrong number of arguments to " .. name)
+            end
+
+            if last_arg then
+                for k, v in pairs(last_arg) do
+                    if not object:set(k, v) then
+                        return
+                    end
+                end
+            end
+
+            print("constructing ...")
+            local operation2 = vips.vips_cache_operation_build(operation);
+            if operation2 == nil then
+                vips.vips_object_unref_outputs(operation)
+                print("build error", object.get_error())
                 return nil
             end
+            operation = operation2
 
-            return pspec[0].value_type
-        end,
-        set = function(object, name, value)
-            print("vobject.set")
-            print("  object =", object)
-            print("  name =", name)
-            print("  value =", value)
+            print("getting output ...")
+            result = {}
+            for i = 1, #arguments do
+                local flags = arguments[i].flags
 
-            local type = object:get_type(name)
-            if not type then
-                print("field", name, "does not exist for object", object)
-                return false
+                if band(flags, OUTPUT) ~= 0 and
+                        band(flags, REQUIRED) ~= 0 and
+                        band(flags, DEPRECATED) == 0 then
+                    result[arguments[i].name] = object:get(arguments[i].name)
+                end
             end
 
-            local gv = gvalue.new()
-            gv:init(object:get_type(name))
-            gv:set(value)
+            if type(last_arg) == "table" then
+                local optional_output = {}
+                for i = 1, #arguments do
+                    local flags = arguments[i].flags
 
-            vips.g_object_set_property(object, name, gv)
+                    if band(flags, OUTPUT) ~= 0 and
+                            band(flags, REQUIRED) == 0 then
+                        optional_output[arguments[i].name] = arguments[i].flags
+                    end
+                end
 
-            return true
-        end,
-        get = function(object, name)
-            print("vobject.get")
-            print("  object =", object)
-            print("  name =", name)
-
-            local type = object:get_type(name)
-            if not type then
-                print("field", name, "does not exist for object", object)
-                return nil
+                for k, v in pairs(last_arg) do
+                    if optional_output[k] then
+                        result[k] = object:get(k)
+                    end
+                end
             end
 
-            local gva = gvalue.newa()
-            gva[0]:init(object:get_type(name))
-            vips.g_object_get_property(object, name, gva)
+            -- if there's a single thing in result, return it without the table
+            -- wrapper
+            local k, v
+            k, v = next(result, nil)
+            if k then
+                local v2
 
-            return gva[0]:get()
-        end,
-        get_error = function ()
-            local errstr = ffi.string(vips.vips_error_buffer())
-            vips.vips_error_clear()
-            return errstr
+                k, v2 = next(result, k)
+                if not k then
+                    print("returning singleton result")
+                    result = v
+                end
+            end
+
+            return result
         end,
 
     }
 }
 
-vobject = ffi.metatype("VipsObject", vobject_mt)
-return vobject
+voperation = ffi.metatype("VipsOperation", voperation_mt)
+return voperation
